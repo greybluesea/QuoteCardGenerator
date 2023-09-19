7355cb50aa766aa64f6fbfe85188fe919b8f759e
Gan (Tony) YUAN <greybluesea@gmail.com>
2023-09-20 11:01:22 +1200

    amplify init

--- app/page.tsx
+++ app/page.tsx
@@ -6,13 +6,16 @@ import QuoteButton from "./components/QuoteButton";
 export default function Home() {
   return (
     <main className=" square-in-the-center z-[2]  drop-shadow-[5px_5px_5px_5px_rgba(222,224,228,0.9)]  ">
-      <h2 className="large-text">Daily Quote Card Generator</h2>
+      <h2 className="large-text">
+        Daily Quote Card<span className="hidden sm:inline">(image/png)</span>{" "}
+        Generator
+      </h2>
       <p
         className=" text-md sm:text-lg md:text-xl lg:text-2xl  text-center"
         style={{ marginTop: "min(5dvw, 5dvh)" }}
       >
         Looking for a splash of inspiration? Generate a quote card/image for
-        today, with text provided by
+        today, with text provided by{" "}
         <Link
           href="https://zenquotes.io/"
           target="_blank"
